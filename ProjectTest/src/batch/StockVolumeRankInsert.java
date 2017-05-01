cd7ad953a4dc7d1add436815125784c326451b42
지찬송 <@DESKTOP-806JV8A>
2017-05-02 02:27:12 +0900

    insert 컬럼 관련 설정 및 jdbc 캐릭터셋 수정

--- ProjectTest/src/batch/StockVolumeRankInsert.java
+++ ProjectTest/src/batch/StockVolumeRankInsert.java
@@ -35,7 +35,7 @@ public class StockVolumeRankInsert {
 	public static void main(String args[]) throws IOException, SQLException{
  
 		kospiVolumeRankInsert();
-		//kosdaqVolumeRankInsert();
+		kosdaqVolumeRankInsert();
 	}
 
 	public static void kospiVolumeRankInsert() throws IOException, SQLException{
@@ -59,34 +59,33 @@ public class StockVolumeRankInsert {
 				sellPrice =  Integer.parseInt(elements.eq(i).select("td").eq(8).text().replace(",", ""));
 				marketCapitalization = Integer.parseInt(elements.eq(i).select("td").eq(9).text().replace(",", ""));
 	
-				InsertStockVolumeRank.insertKospiStockVolumeRnak(dateData, stockDiv, rank, stockName, stockCode, price, changeRate, volume, tradePrice, buyPrice, sellPrice, marketCapitalization);
+				InsertStockVolumeRank.insertStockVolumeRnak(dateData, stockDiv, rank, stockName, stockCode, price, changeRate, volume, tradePrice, buyPrice, sellPrice, marketCapitalization);
 			}
 		}
 	}
 	
-	public static void kosdaqVolumeRankInsert() throws IOException{
+	public static void kosdaqVolumeRankInsert() throws IOException, SQLException{
 		
 		Document doc = Jsoup.connect("http://finance.naver.com/sise/sise_quant.nhn?sosok=1").get();
 		Elements elements = doc.select("div.box_type_l table tbody tr");
 		
-		for(int i = 0; i <= elements.size(); i++){
+        stockDiv = "D";
+        
+        for(int i = 0; i <= elements.size(); i++){
 			if(!elements.eq(i).select(".no").text().equals("")){
-				System.out.println("───────────────────────────────────────");
-				
-				System.out.println("순위 : " + elements.eq(i).select("td").eq(0).text());//순위
-				System.out.println("종목명 : " + elements.eq(i).select("td").eq(1).text());//종목명
-				System.out.println("현재가 : " + elements.eq(i).select("td").eq(2).text());//현재가
-				//System.out.println("전일비 : " + elements.eq(i).select("td").eq(3).text());//전일비
-				System.out.println("등락률 : " + elements.eq(i).select("td").eq(4).text());//등락률
-				System.out.println("거래량 : " + elements.eq(i).select("td").eq(5).text());//거래량
-				System.out.println("거래대금 : " + elements.eq(i).select("td").eq(6).text());//거래대금
-				System.out.println("매수호가 : " + elements.eq(i).select("td").eq(7).text());//매수호가
-				System.out.println("매도호가 : " + elements.eq(i).select("td").eq(8).text());//매도호가
-				System.out.println("시가총액 : " + elements.eq(i).select("td").eq(9).text());//시가총액
-				
-//				for(int j = 0; j <= elements.eq(i).select("td").size(); j++){
-//					System.out.println(elements.eq(i).select("td").eq(j).text());
-//				}
+				dateData = today;
+				rank = elements.eq(i).select("td").eq(0).text();
+				stockName = elements.eq(i).select("td").eq(1).text();
+				stockCode = elements.eq(i).select("td").eq(1).select("a").attr("href").substring(21,26);
+				price = Integer.parseInt(elements.eq(i).select("td").eq(2).text().replace(",", ""));
+				changeRate = Double.parseDouble(elements.eq(i).select("td").eq(4).text().replace("%","")) ;
+				volume = Integer.parseInt(elements.eq(i).select("td").eq(5).text().replace(",", ""));
+				tradePrice = Integer.parseInt(elements.eq(i).select("td").eq(6).text().replace(",", ""));
+				buyPrice = Integer.parseInt(elements.eq(i).select("td").eq(7).text().replace(",", ""));
+				sellPrice =  Integer.parseInt(elements.eq(i).select("td").eq(8).text().replace(",", ""));
+				marketCapitalization = Integer.parseInt(elements.eq(i).select("td").eq(9).text().replace(",", ""));
+	
+				InsertStockVolumeRank.insertStockVolumeRnak(dateData, stockDiv, rank, stockName, stockCode, price, changeRate, volume, tradePrice, buyPrice, sellPrice, marketCapitalization);
 			}
 		}
 	}
